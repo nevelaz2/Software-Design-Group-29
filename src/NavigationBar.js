5a3814c4c68f768614e41fe11e4410cb1adc3744
Gleici Pereira <gpcaresdev@gmail.com>
2024-02-21 16:42:43 -0600

    container component

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom'; 
 import './NavigationBar.css';

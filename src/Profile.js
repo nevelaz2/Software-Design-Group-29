c888e1669219f7beb2e080793e1267b33742c27e
khmocha <127173767+khmocha@users.noreply.github.com>
2024-02-22 13:38:04 -0600

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -1,13 +1,59 @@
 import React from 'react';
 import './App.css';
+import "./Profile.css";
+
+function InfoBox(Props) {
+  let containerStyles = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
+    justifyContent: "start",
+    
+    position: "absolute",
+    left: `${Props.x}px`,
+    top: `${Props.y}px`
+  };
+
+  let textStyles = {
+    color: "red"
+  }
+
+  return (
+    <div className={Props.name} style={containerStyles}>
+      <h1 style={
+        {fontSize: "15px",}
+      }> {Props.title} </h1>
+        {Props.children}
+    </div>
+  );
+}
 
 const Profile = () => {
   return (
-    <div className='profile-page'>
-      <h2>Profile Page</h2>
-      <p>This is the profile page.</p>
-    </div>
+    <>
+      <h1 id="profile-info"> Profile Information </h1>
+      <div className='profile-page'>
+        <div className="container">
+          <InfoBox title="Name" name="info-container" x={50} y={50}>
+            <p> Name Placeholder </p>
+          </InfoBox>
+
+          <InfoBox title="Address" name="info-container" x={50} y={135}>
+            <p> Address Placeholder </p>
+            <p> Country Placeholder </p>
+          </InfoBox>
+
+          <InfoBox title="Username" name="info-container" x={50} y={240}>
+            <p> Your Username Placeholder </p>
+          </InfoBox>
+
+          <InfoBox title="Password" name="info-container" x={50} y={325}>
+            <p> Password Placeholder </p>
+          </InfoBox>
+        </div>
+
+        <button className="button"> Edit </button>
+      </div>
+    </>
   );
 };
-
-export default Profile;

60262095fd2b620b0c03df93a7e0da7c25c2bf4e
nevelaz2 <145055228+nevelaz2@users.noreply.github.com>
2024-02-23 19:53:04 -0600

    Add files via upload
    Added alt text to images

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import './Home.css'
 const Home = () => {
     return (
         <div className="overlay">
-          <img src="/logo.png" className="logo"/>
+          <img src="/logo.png" className="logo" alt='Logo'/>
         </div>
     );
 }

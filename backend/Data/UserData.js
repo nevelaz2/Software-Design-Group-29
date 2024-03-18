8f8db9dbee9ff72dbfd4c2f1f058462cbcabb056
HaqqaniHassaan <157773470+HaqqaniHassaan@users.noreply.github.com>
2024-03-18 00:55:04 -0500

    Update UserData.js (Added name, address, country fields)
    Added the name, address, country fields.

--- backend/Data/UserData.js
+++ backend/Data/UserData.js
@@ -3,15 +3,11 @@ const Mongoose = require("mongoose");
 const Schema = Mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-
-    password: {
-        type: String,
-        required: true
-    }
+   username: { type: String, required: true },
+    password: { type: String, required: true },
+    name: { type: String, required: false },
+    address: { type: String, required: false },
+    country: { type: String, required: false }
 })
 
 const UserData = Mongoose.model("Users", UserSchema, "Users");

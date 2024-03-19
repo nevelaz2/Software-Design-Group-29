7cc85c1bdbad3a521f889b8b59eabc02242c6371
Khang Huynh <khanghuynh@Khangs-MacBook.local>
2024-03-19 00:30:05 -0500

    add fetch request

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,37 @@
 import React, { Fragment } from "react";
+import { useRef } from "react";
 import "../styles/SignUpLogin.css";
 
+const CreateNewData = async (Data, URL) => {
+  const RequestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(Data)
+  }
+
+  try {
+    const Response = await fetch(URL, RequestOptions);
+    const Data = await Response.text();
+
+    console.log(Data);
+
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
 const SignUp = () => {
+  const Username = useRef(null);
+  const Password = useRef(null);
+
+  const HandleSignUp = async (Event) => {
+    Event.preventDefault();
+
+    CreateNewData({username: Username.current.value, password: Password.current.value}, "http://localhost:3001/createuserdata");
+  }
+
   return (
     <Fragment>
       <div class="main-content">
@@ -11,14 +41,14 @@ const SignUp = () => {
           <form id="loginForm">
             <div class="form-group">
               <label for="username">Username</label>
-              <input type="text" id="username" name="username" required />
+              <input ref={Username} type="text" id="username" name="username" required />
             </div>
             <div class="form-group">
               <label for="password">Password</label>
-              <input type="password" id="password" name="password" required />
+              <input ref={Password} type="password" id="password" name="password" required />
             </div>
             <div class="form-actions">
-              <button type="submit" class="btn-signup">Sign Up</button>
+              <button type="submit" class="btn-signup" onClick={HandleSignUp}>Sign Up</button>
             </div>
             <div class="links-container">
               <a href="login.html" class="signin-account">Already have an account? Sign in</a>
